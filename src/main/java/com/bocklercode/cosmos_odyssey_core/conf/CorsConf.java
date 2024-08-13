940a3d92d8b9347238556bacbed7d3690fcdf8f8
Ken Böckler <estken@gmail.com>
2024-08-13 14:34:36 +0300

    cors

---
+++ src/main/java/com/bocklercode/cosmos_odyssey_core/conf/CorsConf.java
@@ -0,0 +1,25 @@
+package com.bocklercode.cosmos_odyssey_core.conf;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConf {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOriginPatterns("http://localhost:5173")
+                        .allowedMethods("GET", "POST")
+                        .allowedHeaders("*")
+                        .allowCredentials(true)
+                        .maxAge(3600);
+            }
+        };
+    }
+}
